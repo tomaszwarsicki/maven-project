dfdd64b91266f96cfada8244f02ea80730b5cd3f
unknown <44004935@E616S7LWYNFUQRP.HBEU.ADROOT.HSBC>
2016-10-17 15:38:56 +0200

    Adder header validation test

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.api.MuleEvent;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
@@ -16,5 +17,11 @@ public class HelloMavenTest extends FunctionalTestCase {
     protected String getConfigFile() {
         return "maven-project.xml";
     }
+    
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+    	   MuleEvent event = runFlow("retrieveFlights");
+    	   String contentType = event.getMessage().getOutboundProperty("Content-Type");
+    	   assertEquals("application/json", contentType);
+    	 }
 
 }
